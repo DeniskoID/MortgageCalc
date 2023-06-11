7d45c886327587bea862be4995adf92691ffd3fa
Denis <80954428+DeniskoID@users.noreply.github.com>
2023-06-11 12:05:48 +0300

    Adds function updateResultsView new file:   js/view/updateResultsView.js

--- js/model.js
+++ js/model.js
@@ -8,8 +8,16 @@ let data = {
   },
 };
 
+let results = {
+  rate: data.selectedProgram,
+};
+
 function getData() {
   return { ...data };
+}
+
+function getResults() {
+  return { ...results };
 }
 
 function setData(newData) {
@@ -17,6 +25,13 @@ function setData(newData) {
     ...data,
     ...newData,
   };
+
+  results = {
+    rate: data.selectedProgram,
+  };
+
+  console.log('Updated data', data);
+  console.log('New results', results);
 }
 
-export { getData, setData };
+export { getData, setData, getResults };
